098543008a389949d3370bf355cbdd928b3cbf4a
Bryce <egymlol@gmail.com>
2023-02-27 23:51:55 +1100

    - Implemented AnimIterateSprites

--- Assets/Project/Scripts/Transport/Drill.cs
+++ Assets/Project/Scripts/Transport/Drill.cs
@@ -4,32 +4,5 @@ using UnityEngine;
 
 public class Drill : MonoBehaviour
 {
-    [SerializeField] private int framesPerSprite;
-    [SerializeField] private Sprite[] sprites;
-
-    private SpriteRenderer sr;
-    private int counter;
-    private int currSprite;
-
-    private void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-
-        counter = 0;
-        currSprite = 0;
-    }
-
-    private void Update()
-    {
-        counter += 1;
-
-        if (counter >= framesPerSprite)
-        {
-            counter = 0;
-            currSprite += 1;
-            if (currSprite >= sprites.Length) { currSprite = 0; }
-
-            sr.sprite = sprites[currSprite];
-        }
-    }
+    
 }
