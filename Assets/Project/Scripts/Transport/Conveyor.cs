f9d2cdcbf33923837b450351bcb1d90d71107161
Bryce <egymlol@gmail.com>
2023-03-01 10:49:41 +1100

    - Removed old Conveyor and MapGen classes

--- Assets/Project/Scripts/Transport/Conveyor.cs
+++ Assets/Project/Scripts/Transport/Conveyor.cs
@@ -2,182 +2,69 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using UnityEditor;
 
 [RequireComponent(typeof(TSystemRotator))]
 [RequireComponent(typeof(TSystemConnector))]
-[RequireComponent(typeof(TSystemQueueReceiver))]
+[RequireComponent(typeof(TSystemBeltReceiver))]
 public class Conveyor : MonoBehaviour
 {
-    /*
-        For a 5 block Conveyor:
-
-        back    |   x o o x x   |   front (first slot if you dequeue)
-        back    |   4 3 2 1 0   |   front (first slot if you dequeue)
-
-        Travelling in >>>> direction
-    */
-
-    // potentially to think of a way that this movespeed doesn't scale with the amount of slots
-    // currently if you bump up the amount of slots, items take significantly longer 
-    // to move across a single conveyor
-    public float ItemsMovedPerSecond;
-
-    private int stackedItems = 0;
+    public float Movespeed;
 
     private TSystemConnector connector;
-    private TSystemQueueReceiver receiver;
-
-    [Header("Debug")]
-    [SerializeField] private bool showSlotGraphics;
+    private TSystemBeltReceiver receiver;
 
     private void Awake()
     {
         connector = GetComponent<TSystemConnector>();
-        receiver = GetComponent<TSystemQueueReceiver>();
-    }
-
-    private void Start()
-    {
-        CreateAndPlaceSlots();
-        EstablishSlotQueue();
+        receiver = GetComponent<TSystemBeltReceiver>();
     }
 
     private void FixedUpdate()
     {
-        ToggleSlotSprites();
-        MoveSlotsAlongConveyor();
+        MoveItems();
     }
 
-    // ITEM MOVEMENT
-
-    private void MoveSlotsAlongConveyor()
+    private void MoveItems()
     {
-        Vector3 _moveOffset = new Vector3((ItemsMovedPerSecond / receiver.SlotCount) * Time.deltaTime, 0f, 0f);
-        Vector3 _resetPos = new Vector3(-0.5f, 0f, 0f);
+        if (receiver.Items.Count == 0) { return; }
 
-        stackedItems = CountStackedItems();
-        float _xResetThreshold = 0.5f - ((1f / receiver.SlotCount) * stackedItems);
+        Vector3 _moveOffset = new Vector3(Movespeed * Time.deltaTime, 0f, 0f);
+        float _rightEdge = 0.5f;
 
-        List<ConveyorSlot> _slotCache = new List<ConveyorSlot>();
-        ConveyorSlot _resetSlot = null;
-        ConveyorSlot _front = receiver.Slots.Peek();
-
-        for (int i = 0; i <= receiver.SlotCount - 1; i++)
+        for (int i = 0; i < receiver.Items.Count; i++)
         {
-            ConveyorSlot _slot = receiver.Slots.Dequeue();
+            var _item = receiver.Items[i];
+            float _currXPos = _item.transform.localPosition.x;
+            float _inFrontXPos = 0f;
 
-            // if slot is part of the front stack
-            if (i < stackedItems)
+            // if not at the front, cache the pos of the item in front for spacing
+            if (i != 0)
             {
-                if (_slot == _front && connector.CanOffloadItem(_slot.Item))
-                {
-                    connector.GetConnectedReceiver().Give(_slot.Item);
-                    _slot.ClearItem();
-                }
-
-                _slotCache.Add(_slot);
-                continue;
+                _inFrontXPos = receiver.Items[i - 1].transform.localPosition.x;
             }
 
-            // handling the rest
-            if (_slot.transform.localPosition.x >= _xResetThreshold)
+            if (connector.CanOffloadItem(_item))
             {
-                _slot.transform.localPosition = _resetPos;
-                _slot.transform.localPosition += _moveOffset;
-                _resetSlot = _slot;
+                if (i == 0 && (_currXPos >= _rightEdge))
+                {
+                    connector.GetConnectedReceiver().Give(_item);
+                    receiver.Items.Remove(_item);
+                }
+
+                if (i != 0 && ((_inFrontXPos - _currXPos) < receiver.gapWidth)) { continue; }
+                _item.transform.localPosition += _moveOffset;
             }
             else
             {
-                _slot.transform.localPosition += _moveOffset;
-                _slotCache.Add(_slot);
-            }
-        }
+                if (_currXPos >= (_rightEdge - (receiver.gapWidth * (i + 1)))) 
+                { 
+                    _rightEdge -= receiver.gapWidth;
+                    continue;
+                }
 
-        // ENQUEUE SLOTS IS NEW ORDER
-        foreach (ConveyorSlot _slot in _slotCache)
-        {
-            receiver.Slots.Enqueue(_slot);
-        }
-
-        if (_resetSlot != null)
-        {
-            receiver.Slots.Enqueue(_resetSlot);
-        }
-    }
-
-    private int CountStackedItems()
-    {
-        int _stackedItems = 0;
-
-        for (int i = 0; i <= receiver.SlotCount - 1; i++)
-        {
-            ConveyorSlot _slot = receiver.Slots.Dequeue();
-
-            if (_slot.IsNotEmpty() && _stackedItems == i)
-            {
-                _stackedItems += 1;
-            }
-
-            receiver.Slots.Enqueue(_slot);
-        }
-
-        return _stackedItems;
-    }
-
-    private void CreateAndPlaceSlots()
-    {
-        GameObject _slotPrefab = GetComponentInChildren<ConveyorSlot>().gameObject;
-        _slotPrefab.transform.position = transform.position + new Vector3(-0.5f + ((1f / receiver.SlotCount) * (receiver.SlotCount + 1f)), 0f, 0f);
-
-        for (int i = 0; i < receiver.SlotCount - 1; i++)
-        {
-            Vector3 _positionOffset = new Vector3(-0.5f + ((1f / receiver.SlotCount) * (i + 1f)), 0f, 0f);
-            GameObject slotObj = Instantiate(_slotPrefab, transform.position + _positionOffset, Quaternion.identity);
-            slotObj.transform.parent = transform;
-        }
-
-        ConveyorSlot[] _slots = GetComponentsInChildren<ConveyorSlot>();
-
-        for (int i = 0; i < receiver.SlotCount; i++)
-        {
-            _slots[i].transform.name = _slotPrefab.name + "_" + i;
-            _slots[i].transform.localScale = new Vector3(
-                _slots[i].transform.localScale.x / receiver.SlotCount,
-                _slots[i].transform.localScale.y,
-                _slots[i].transform.localScale.z);
-        }
-    }
-
-    private void EstablishSlotQueue()
-    {
-        receiver.Slots = new Queue<ConveyorSlot>(receiver.SlotCount);
-
-        ConveyorSlot[] _slots = GetComponentsInChildren<ConveyorSlot>();
-        for (int i = receiver.SlotCount - 1; i >= 0; i--)
-        {
-            receiver.Slots.Enqueue(_slots[i])   ;
-        }
-    }
-
-    private void ToggleSlotSprites()
-    {
-        if (showSlotGraphics)
-        {
-            foreach (ConveyorSlot slot in receiver.Slots)
-            {
-                slot.ShowSprite();
-            }
-        }
-        else
-        {
-            foreach (ConveyorSlot slot in receiver.Slots)
-            {
-                slot.HideSprite();
+                if (i != 0 && ((_inFrontXPos - _currXPos) < receiver.gapWidth)) { continue; }
+                _item.transform.localPosition += _moveOffset;
             }
         }
     }
-
-
 }
